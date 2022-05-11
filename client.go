e533ff90e2c30d0c5a7e46f4fc6fca7b3d6d0d47
brendanta <brendan.ta@jacques.com.au>
2022-05-11 13:55:48 +1000

    Extend client so it supports buffered channel responses
    Add helper functions to allow opening the device with a mask of
    channels, read from the device, and close the device.

--- client.go
+++ client.go
@@ -33,6 +33,61 @@ func New(remote string) (*IIO, error) {
 	return i, nil
 }
 
+// Sends command and check whether return matches return length.
+// Expects 3 return arguments; response length, enabled channel buffers,
+// and response
+func (i *IIO) bufferSizedReply(cmd string) (*[]uint8, error) {
+	log.Debugf("bufferSizedReply(%s)", cmd)
+	_, err := i.writer.WriteString(fmt.Sprintf("%s\n", cmd))
+	if err != nil {
+		return nil, fmt.Errorf("error writing: %v", err)
+	}
+
+	if err := i.writer.Flush(); err != nil {
+		return nil, fmt.Errorf("error flushing: %v", err)
+	}
+
+	log.Debugf("reading reply")
+	sizeStr, err := i.reader.ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("error reading size: %v", err)
+	}
+	sizeStr = strings.TrimRight(sizeStr, "\n")
+
+	size, err := strconv.Atoi(sizeStr)
+	if err != nil {
+		return nil, fmt.Errorf("error converting size '%s' into int: %v", sizeStr, err)
+	}
+	log.Debugf("size is %d", size)
+
+	buffer := make([]byte, size)
+	if size < 0 {
+		return nil, fmt.Errorf("received negative size (error) from remote: %d", size)
+	} else if size == 0 {
+		return &buffer, nil
+	}
+
+	// We do not care/check the channel buffers however we want to ensure it is at least
+	// present to confirm that the entire response in in the format we expect
+	_, err = i.reader.ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("error reading channel size: %v", err)
+	}
+
+	numread, err := i.reader.Read(buffer)
+	if err != nil {
+		return nil, fmt.Errorf("error reading data: %v", err)
+	}
+
+	if size != numread {
+		return nil, fmt.Errorf("expected %d bytes, got %d", size, numread)
+	}
+
+	return &buffer, nil
+}
+
+// Sends command and check whether return matches return length.
+// Expects 2 return arguments; response length and response
 func (i *IIO) commandSizedReply(cmd string) (*string, error) {
 	log.Debugf("commandSizedReply(%s)", cmd)
 	_, err := i.writer.WriteString(fmt.Sprintf("%s\n", cmd))
@@ -57,11 +112,17 @@ func (i *IIO) commandSizedReply(cmd string) (*string, error) {
 	}
 	log.Debugf("size is %d", size)
 
+	// commandSizedReply returns the size of the response and then the response
+	// for commands such as OPEN which returns only the size i.e. 0 with no response
+	// this forever waits for the end of line character, we therefore associate 0 as
+	// a successful response with no return contents.
+	reply := ""
 	if size < 0 {
 		return nil, fmt.Errorf("received negative size (error) from remote: %d", size)
+	} else if size == 0 {
+		return &reply, nil
 	}
-
-	reply, err := i.reader.ReadString('\n')
+	reply, err = i.reader.ReadString('\n')
 	if err != nil {
 		return nil, fmt.Errorf("error reading data: %v", err)
 	}
@@ -92,6 +153,7 @@ func (i *IIO) fetchContext() error {
 	return nil
 }
 
+// Fetch all attributes for all devices
 func (i *IIO) FetchAttributes() error {
 	for _, device := range i.Context.Devices {
 		for _, channel := range device.Channels {
@@ -115,6 +177,37 @@ func (i *IIO) FetchAttributes() error {
 				attribute.Value = f
 			}
 		}
+	}
+
+	return nil
+}
+
+func (i *IIO) CreateBuffer(dev_name string, buf_size int, chan_mask string) error {
+
+	_, err := i.commandSizedReply(fmt.Sprintf("OPEN %s %d %s", dev_name, buf_size, chan_mask))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i *IIO) DumpBuffer(dev_name string, buf_size int) ([]uint8, error) {
+
+	var val *[]uint8
+	val, err := i.bufferSizedReply(fmt.Sprintf("READBUF %s %d", dev_name, buf_size))
+	if err != nil {
+		return *val, err
+	}
+
+	return *val, nil
+}
+
+func (i *IIO) DestroyBuffer(dev_name string) error {
+
+	_, err := i.commandSizedReply(fmt.Sprintf("CLOSE %s", dev_name))
+	if err != nil {
+		return err
 	}
 
 	return nil
